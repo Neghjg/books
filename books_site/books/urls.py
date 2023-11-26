d52bc3aa53f281e79d39265d6ec8371b50189a1e
Neghjg <neghjg@yandex.ru>
2023-11-26 21:33:38 +0300

    add bootstrap

--- books_site/books/urls.py
+++ books_site/books/urls.py
@@ -4,5 +4,10 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('adventures/', adventures, name= 'adventures')
+    path('adventures/', adventures, name= 'adventures'),
+    path('detective/', detective, name= 'detective'),
+    path('fantasy/', fantasy, name= 'fantasy'),
+    path('classical_prose/', classical_prose, name= 'classical_prose'),
+    path('modern_prose/', modern_prose, name= 'modern_prose'),
+    
 ]
--- books_site/books/views.py
+++ books_site/books/views.py
@@ -9,6 +9,21 @@ def index(request):
 
 
 def adventures(request):
-    
     books = Book.objects.filter(cat = 5)
     return render(request, 'books/adventures.html', {'books': books})
+
+def detective(request):
+    books = Book.objects.filter(cat = 4)
+    return render(request, 'books/detective.html', {'books': books})
+
+def fantasy(request):
+    books = Book.objects.filter(cat = 3)
+    return render(request, 'books/fantasy.html', {'books': books})
+
+def classical_prose(request):
+    books = Book.objects.filter(cat = 2)
+    return render(request, 'books/classical_prose.html', {'books': books})
+
+def modern_prose(request):
+    books = Book.objects.filter(cat = 1)
+    return render(request, 'books/modern_prose.html', {'books': books})
