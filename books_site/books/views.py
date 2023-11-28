c0f1588cb6215c57de15cce15647435ab1456e1e
Neghjg <neghjg@yandex.ru>
2023-11-28 17:46:25 +0300

    fix bootsrap

--- books_site/books/views.py
+++ books_site/books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+import requests
 
 
 # Create your views here.
@@ -9,21 +10,51 @@ def index(request):
 
 
 def adventures(request):
-    books = Book.objects.filter(cat = 5)
+    sort = request.GET.get('sort', 'default')
+    if sort == 'default':
+        books = Book.objects.filter(cat = 5)
+    elif sort == 'price_to_low':
+        books = Book.objects.filter(cat = 5).order_by('price')
+    elif sort == 'price_to_hight':
+        books = Book.objects.filter(cat = 5).order_by('-price')
     return render(request, 'books/adventures.html', {'books': books})
 
 def detective(request):
-    books = Book.objects.filter(cat = 4)
+    sort = request.GET.get('sort', 'default')
+    if sort == 'default':  
+        books = Book.objects.filter(cat = 4)
+    elif sort == 'price_to_low':
+        books = Book.objects.filter(cat = 4).order_by('price')
+    elif sort == 'price_to_hight':
+        books = Book.objects.filter(cat = 4).order_by('-price')
     return render(request, 'books/detective.html', {'books': books})
 
 def fantasy(request):
-    books = Book.objects.filter(cat = 3)
+    sort = request.GET.get('sort', 'default')
+    if sort == 'default':  
+        books = Book.objects.filter(cat = 3)
+    elif sort == 'price_to_low':
+        books = Book.objects.filter(cat = 3).order_by('price')
+    elif sort == 'price_to_hight':
+        books = Book.objects.filter(cat = 3).order_by('-price')
     return render(request, 'books/fantasy.html', {'books': books})
 
 def classical_prose(request):
-    books = Book.objects.filter(cat = 2)
+    sort = request.GET.get('sort', 'default')
+    if sort == 'default':  
+        books = Book.objects.filter(cat = 2)
+    elif sort == 'price_to_low':
+        books = Book.objects.filter(cat = 2).order_by('price')
+    elif sort == 'price_to_hight':
+        books = Book.objects.filter(cat = 2).order_by('-price')
     return render(request, 'books/classical_prose.html', {'books': books})
 
 def modern_prose(request):
-    books = Book.objects.filter(cat = 1)
+    sort = request.GET.get('sort', 'default')
+    if sort == 'default':  
+        books = Book.objects.filter(cat = 1)
+    elif sort == 'price_to_low':
+        books = Book.objects.filter(cat = 1).order_by('price')
+    elif sort == 'price_to_hight':
+        books = Book.objects.filter(cat = 1).order_by('-price')
     return render(request, 'books/modern_prose.html', {'books': books})
