3931275fa79d6767ae71d2c33efb33acaa82ce14
Neghjg <neghjg@yandex.ru>
2024-02-29 13:03:15 +0300

    add save messages

--- books_site/chat/consumers.py
+++ books_site/chat/consumers.py
@@ -39,6 +39,7 @@ import requests
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 from django.utils import timezone
+from chat.models import ChatMessage2, UserMessege2
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -46,6 +47,10 @@ class ChatConsumer(WebsocketConsumer):
         self.user = self.scope['user']
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
+        self.room = ChatMessage2.objects.get(id=self.room_name)
+        
+        
+        
 
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
@@ -53,6 +58,13 @@ class ChatConsumer(WebsocketConsumer):
         )
 
         self.accept()
+        
+        
+        message = UserMessege2.objects.filter(chat_room=self.room).order_by("-id")[:10]
+        for mes in reversed(message):
+            self.send(text_data=json.dumps({
+                "message": mes.text
+            }))
 
     def disconnect(self, close_code):
         # Leave room group
@@ -64,7 +76,8 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-
+        #UserMessege2.objects.create(chat_room=self.room_name, user=self.user, text=message)
+        UserMessege2.objects.create(chat_room=self.room, user=self.user, text=message)
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name, {"type": "chat.message", "message": message,
